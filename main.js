f9ec139f59b9e7621aca71996409d608a113ee88
Andra Malmen <andra.malmen@tieto.com>
2020-05-02 19:31:56 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+const listRepos = async (username) => {
+    const repos = await fetch(
+        `https://api.github.com/users/${username}/repos?type=owners&sort=updated`
+    )
+        .then((res) => res.json())
+        .catch((error) => console.error(error));
+
+    const markup = repos
+        .map(
+            (repo) =>
+                `<li>
+                    <a href="${repo.html_url}">${repo.name}</a>
+                    (✨${repo.stargazers_count})
+                </li>`
+        )
+        .join('');
+
+    const content = document.getElementById('content');
+    console.log(markup);
+    content.innerHTML = `<ul>${markup}</ul>`;
+};
+
+listRepos('andramalmen');
